e45cc5d826002bc561a39da8bae4e8dc2e328496
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2025-09-05 18:38:17 +0800

    Initial Commit

---
+++ ChronoGraph/ChronoGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoGraphApp.swift
+//  ChronoGraph
+//
+//  Created by Shiyao Zhang on 05/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChronoGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChronoGraphTests/ChronoGraphTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoGraphTests.swift
+//  ChronoGraphTests
+//
+//  Created by Shiyao Zhang on 05/09/2025.
+//
+
+import Testing
+@testable import ChronoGraph
+
+struct ChronoGraphTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
